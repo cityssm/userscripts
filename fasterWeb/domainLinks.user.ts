8fb3bac87980d3a097a2e89f7c08360e6b0ecdab
Dan Gowans <dangowans@users.noreply.github.com>
2024-04-18 14:53:31 -0400

    initial commit

---
+++ fasterWeb/domainLinks.user.ts
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name        FASTER Web - Domain Tabs
+// @namespace   https://github.com/cityssm/userscripts
+// @match       https://*.fasterwebcloud.com/FASTER/*
+// @grant       none
+// @version     1.0
+// @author      The Corporation of the City of Sault Ste. Marie
+// @description Updates domain tabs to default to more useful pages.
+// @run-at      document-end
+// ==/UserScript==
+
+;(() => {
+  const assetsDomainLink = document.querySelector(
+    '#ctl00_DomainMenu_RadMenuDomain a[href$="/Assets/Default.aspx"]'
+  ) as HTMLAnchorElement | null
+  if (assetsDomainLink) {
+    assetsDomainLink.href =
+      assetsDomainLink.href.slice(0, -12) + 'SelectAsset/Default.aspx'
+  }
+
+  const inventoryDomainLink = document.querySelector(
+    '#ctl00_DomainMenu_RadMenuDomain a[href$="/Parts/Default.aspx"]'
+  ) as HTMLAnchorElement | null
+  if (inventoryDomainLink) {
+    inventoryDomainLink.href =
+      inventoryDomainLink.href.slice(0, -12) + 'PartList/Default.aspx'
+  }
+
+  const accountingDomainLink = document.querySelector(
+    '#ctl00_DomainMenu_RadMenuDomain a[href$="/Accounting/Default.aspx"]'
+  ) as HTMLAnchorElement | null
+  if (accountingDomainLink) {
+    accountingDomainLink.href =
+      accountingDomainLink.href.slice(0, -12) + 'Search/Default.aspx'
+  }
+})()
