6abbf22cf64334e239e13b32ff8bbe5640092868
Dan Gowans <dangowans@users.noreply.github.com>
2024-04-22 13:07:50 -0400

    show link on all home domain pages

--- fasterWeb/homeLinkToUserscripts.user.js
+++ fasterWeb/homeLinkToUserscripts.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         FASTER Web - Home - Link to Userscripts
 // @namespace    https://github.com/cityssm/userscripts
-// @match        https://*.fasterwebcloud.com/FASTER/Domains/Home/Default.aspx
+// @match        https://*.fasterwebcloud.com/FASTER/Domains/Home/*.aspx
 // @grant        none
-// @version      1.0.0
+// @version      1.0.1
 // @author       The Corporation of the City of Sault Ste. Marie
 // @description  Adds a link to the City's Userscripts page to the Actions menu.
 // @run-at       document-end
