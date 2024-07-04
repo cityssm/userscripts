1ad87c71a2d841d994204bbb5340a8c754a90623
Dan Gowans <dangowans@users.noreply.github.com>
2024-07-04 09:37:31 -0400

    redirect to advanced search pages

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ It can be used to track assets, maintenance on those assets, and parts inventory
 ### All Sections
 
 📜 [Domain Tabs](https://github.com/cityssm/userscripts/raw/main/fasterWeb/domainLinks.user.js)<br />
-Updates domain tabs to default to more useful pages.
+Updates domain tabs to default to Advanced Search pages.
 
 📜 [Focus Outlines](https://github.com/cityssm/userscripts/raw/main/fasterWeb/focusOutlines.user.js)<br />
 Increases accessibility by outlining actionable elements on hover and keyboard focus.
--- fasterWeb/domainLinks.user.js
+++ fasterWeb/domainLinks.user.js
@@ -3,9 +3,9 @@
 // @namespace    https://github.com/cityssm/userscripts
 // @match        https://*.fasterwebcloud.com/FASTER/*
 // @grant        none
-// @version      1.0.0
+// @version      1.1.0
 // @author       The Corporation of the City of Sault Ste. Marie
-// @description  Updates domain tabs to default to more useful pages.
+// @description  Updates domain tabs to default to Advanced Search pages.
 // @run-at       document-end
 // @downloadURL  https://raw.githubusercontent.com/cityssm/userscripts/main/fasterWeb/domainLinks.user.js
 // @supportURL   https://github.com/cityssm/userscripts/issues
@@ -17,15 +17,27 @@
     const domainLinksToUpdate = [
         [
             '#ctl00_DomainMenu_RadMenuDomain a[href$="/Assets/Default.aspx"]',
-            'SelectAsset/Default.aspx'
+            'Search/Default.aspx'
         ],
         [
             '#ctl00_DomainMenu_RadMenuDomain a[href$="/Parts/Default.aspx"]',
-            'PartList/Default.aspx'
+            'Search/Default.aspx'
+        ],
+        [
+            '#ctl00_DomainMenu_RadMenuDomain a[href$="/Maintenance/Default.aspx"]',
+            'WorkOrder/Search/Default.aspx'
+        ],
+        [
+            '#ctl00_DomainMenu_RadMenuDomain a[href$="/Fuel/Default.aspx"]',
+            'Search/Default.aspx'
         ],
         [
             '#ctl00_DomainMenu_RadMenuDomain a[href$="/Accounting/Default.aspx"]',
             'Search/Default.aspx'
+        ],
+        [
+            '#ctl00_DomainMenu_RadMenuDomain a[href$="/Vendors/Default.aspx"]',
+            'Search/Default.aspx?str='
         ]
     ];
     for (const domainLinkToUpdate of domainLinksToUpdate) {
