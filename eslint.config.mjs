391e65e47fa90bb602778f0581c7513a4f595e70
Dan Gowans <dangowans@users.noreply.github.com>
2024-09-05 15:55:43 -0400

    fix type error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,12 @@
 import eslintConfigCityssm from 'eslint-config-cityssm';
-export default [
-    ...eslintConfigCityssm,
-    {
-        languageOptions: {
-            parserOptions: {
-                project: ['./tsconfig.json', './tsconfig.client.json']
-            }
-        },
-        rules: {
-            'no-secrets/no-secrets': 'off'
+import tseslint from 'typescript-eslint';
+export default tseslint.config(...eslintConfigCityssm, {
+    languageOptions: {
+        parserOptions: {
+            project: ['./tsconfig.json', './tsconfig.client.json']
         }
+    },
+    rules: {
+        'no-secrets/no-secrets': 'off'
     }
-];
+});
--- eslint.config.mts
+++ eslint.config.mts
@@ -1,15 +1,13 @@
 import eslintConfigCityssm from 'eslint-config-cityssm'
+import tseslint, { type Config } from 'typescript-eslint'
 
-export default [
-  ...eslintConfigCityssm,
-  {
-    languageOptions: {
-      parserOptions: {
-        project: ['./tsconfig.json', './tsconfig.client.json']
-      }
-    },
-    rules: {
-      'no-secrets/no-secrets': 'off'
+export default tseslint.config(...eslintConfigCityssm, {
+  languageOptions: {
+    parserOptions: {
+      project: ['./tsconfig.json', './tsconfig.client.json']
     }
+  },
+  rules: {
+    'no-secrets/no-secrets': 'off'
   }
-]
+}) as Config
