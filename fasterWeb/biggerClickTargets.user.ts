4c657603edf0c59cc0693e8587f47e5cc19007bc
Dan Gowans <dangowans@users.noreply.github.com>
2024-09-10 09:31:05 -0400

    version bump

--- fasterWeb/biggerClickTargets.user.ts
+++ fasterWeb/biggerClickTargets.user.ts
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/cityssm/userscripts
 // @match        https://*.fasterwebcloud.com/FASTER/*
 // @grant        GM_addStyle
-// @version      0.1.0-dev
+// @version      0.2.0-dev
 // @author       The Corporation of the City of Sault Ste. Marie
 // @description  Increases the size of some click targets, increasing usability on smaller screens.
 // @run-at       document-end
