32aee5f7b259f21f2488c54526629b2b2f4b9096
Dan Gowans <dangowans@users.noreply.github.com>
2024-09-11 12:38:12 -0400

    set higher viewport width for certain pages

--- fasterWeb/setViewport.user.js
+++ fasterWeb/setViewport.user.js
@@ -3,10 +3,10 @@
 // @namespace    https://github.com/cityssm/userscripts
 // @match        https://*.fasterwebcloud.com/FASTER/*
 // @grant        none
-// @version      0.1.0-dev
+// @version      0.2.0-dev
 // @author       The Corporation of the City of Sault Ste. Marie
 // @description  Attempts to reduce the amount of zooming required on smaller screens.
-// @run-at       document-end
+// @run-at       document-body
 // @downloadURL  https://raw.githubusercontent.com/cityssm/userscripts/main/fasterWeb/setViewport.user.js
 // @supportURL   https://github.com/cityssm/userscripts/issues
 // @homepageURL  https://cityssm.github.io/userscripts/
@@ -14,7 +14,24 @@
 // ==/UserScript==
 ;
 (() => {
+    const defaultViewportWidth = 800;
+    const specialViewportWidth = 1100;
+    const specialPages = [
+        '/domains/fuel/search/default.aspx',
+        '/domains/maintenance/directcharge/search/default.aspx',
+        '/domains/maintenance/workorder/search/default.aspx'
+    ];
     if (document.querySelector('meta[name="viewport"]') === null) {
-        document.head.insertAdjacentHTML('beforeend', `<meta name="viewport" content="width=750, initial-scale=1" />`);
+        const currentPage = window.location.pathname.toLowerCase();
+        let viewportWidth = defaultViewportWidth;
+        for (const specialPage of specialPages) {
+            console.log(specialPage);
+            if (currentPage.includes(specialPage)) {
+                console.log('special: ' + specialPage);
+                viewportWidth = specialViewportWidth;
+                break;
+            }
+        }
+        document.head.insertAdjacentHTML('beforeend', `<meta name="viewport" content="width=${viewportWidth}, initial-scale=1" />`);
     }
 })();
