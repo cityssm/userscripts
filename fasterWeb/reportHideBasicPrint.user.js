f32f4753ad883b935b7b7441d23e3253f6abc745
Dan Gowans <dangowans@users.noreply.github.com>
2024-09-23 14:53:28 -0400

    clean up unneeded condition

--- fasterWeb/reportHideBasicPrint.user.js
+++ fasterWeb/reportHideBasicPrint.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/cityssm/userscripts
 // @match        https://*.fasterwebcloud.com/FASTER/Domains/Reports/ReportViewer.aspx
 // @grant        none
-// @version      1.2.0
+// @version      1.2.1
 // @author       The Corporation of the City of Sault Ste. Marie
 // @description  Removes the unsupported "Basic Print" button.
 // @run-at       document-end
@@ -15,8 +15,7 @@
 ;
 (() => {
     const printButtonElement = document.querySelector('#PrintRadButton');
-    if (printButtonElement !== null &&
-        printButtonElement !== undefined) {
+    if (printButtonElement !== null) {
         printButtonElement.style.opacity = '0';
         printButtonElement.style.width = '0px';
         printButtonElement.style.display = 'none';
--- fasterWeb/reportHideBasicPrint.user.ts
+++ fasterWeb/reportHideBasicPrint.user.ts
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/cityssm/userscripts
 // @match        https://*.fasterwebcloud.com/FASTER/Domains/Reports/ReportViewer.aspx
 // @grant        none
-// @version      1.2.0
+// @version      1.2.1
 // @author       The Corporation of the City of Sault Ste. Marie
 // @description  Removes the unsupported "Basic Print" button.
 // @run-at       document-end
@@ -14,13 +14,11 @@
 // ==/UserScript==
 
 ;(() => {
-  const printButtonElement =
-    document.querySelector('#PrintRadButton') as HTMLElement | null
+  const printButtonElement = document.querySelector(
+    '#PrintRadButton'
+  ) as HTMLElement | null
 
-  if (
-    printButtonElement !== null &&
-    printButtonElement !== undefined
-  ) {
+  if (printButtonElement !== null) {
     printButtonElement.style.opacity = '0'
     printButtonElement.style.width = '0px'
     printButtonElement.style.display = 'none'
